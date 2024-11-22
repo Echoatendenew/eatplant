f0a8f716df04a4c6bdef9da8d57e74120ceaa673
Fabriccio Fortuna <echomaisoficial@gmail.com>
2024-11-22 14:14:26 -0300

    first commit

---
+++ backend/src/services/QuickMessageService/FindService.ts
@@ -0,0 +1,42 @@
+import { Op } from "sequelize";
+import QuickMessage from "../../models/QuickMessage";
+import Company from "../../models/Company";
+import User from "../../models/User";
+
+
+type Params = {
+  companyId: string;
+  userId: string;
+  showAll: string;
+};
+
+const FindService = async ({ companyId, userId, showAll }: Params): Promise<QuickMessage[]> => {
+
+  const userProfile = (await User.findByPk(userId))?.profile;
+
+  let notes: QuickMessage[]
+  if(userProfile !== 'admin'){
+    notes =  await QuickMessage.findAll({
+     where: {
+       companyId,
+       geral: true,
+       ...(showAll === "true" ? {} : { isCategory: false })
+     },
+     include: [{ model: Company, as: "company", attributes: ["id", "name"] }],
+     order: [["shortcode", "ASC"]]
+   });
+  }else{
+    notes =  await QuickMessage.findAll({
+      where: {
+        companyId,
+        ...(showAll === "true" ? {} : { isCategory: false })
+      },
+      include: [{ model: Company, as: "company", attributes: ["id", "name"] }],
+      order: [["shortcode", "ASC"]]
+    });
+  }
+
+  return notes;
+};
+
+export default FindService;
